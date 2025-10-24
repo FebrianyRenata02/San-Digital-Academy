af084f4a85a4363aabe3dd2b0c30939f47e50188
Febriany Renata <96133771+FebrianyRenata02@users.noreply.github.com>
2025-10-24 17:01:31 +0700

    Update Developer.js

--- Developer.js
+++ Developer.js
@@ -33,9 +33,9 @@ document.body.innerHTML += `
         <img src="/img/javascript-frameworks-animate.svg" alt="UI UX Illustration">
       </div>
       <p>
-        <strong>💻 Jenis Web Developer</strong>
+        <strong>💡 Jenis Web Developer</strong>
         <br><br>
-        <strong>Front-End Developer</strong> 
+        <strong>🔮 1. Front-End Developer</strong> 
         <br><br>
         Fokus pada tampilan visual dan interaksi pengguna.
         <br><br>
@@ -45,13 +45,50 @@ document.body.innerHTML += `
       </p>
           <br><br>
       <p>
-       <strong> 2. Back-End Developer </strong>
+       <strong> 🧿 2. Back-End Developer </strong>
         <br><br>
         Mengurus bagian server, database, dan logika aplikasi.
          <br><br>
         Menggunakan bahasa seperti Node.js, PHP, Python, Java, atau Ruby.
          <br><br>
         Contoh tugas: menangani login pengguna, penyimpanan data, dan komunikasi antar sistem.
+      </p>
+          <br><br> 
+      <p>
+       <strong> 👨‍💻 3. Full-Stack Developer </strong>
+        <br><br> 
+       Menguasai baik front-end maupun back-end.
+         <br><br>
+       Mampu membangun website secara keseluruhan dari nol.
+      
+      </p>
+          <br><br> 
+      <p>
+       <strong> ⚙️ Tugas Utama Web Developer </strong>
+        <br><br> 
+      Membuat dan menguji kode website.
+         <br><br>
+       Mengoptimalkan kecepatan dan keamanan situs.
+      <br><br>    
+        Memastikan website bisa digunakan di semua browser dan perangkat.
+       <br><br>
+       Berkolaborasi dengan desainer UI/UX untuk membuat tampilan yang menarik.
+       <br><br>
+       Memperbaiki bug dan memperbarui fitur website.
+      </p>
+      <br><br> 
+      <p>
+       <strong> 🧠 Skill yang Dibutuhkan </strong>
+        <br><br> 
+    HTML, CSS, JavaScript
+         <br><br>
+       Framework seperti Bootstrap, React, Vue, atau Angular
+      <br><br>    
+       Pengetahuan tentang server dan database (misalnya MySQL, MongoDB)
+       <br><br>
+      Pemahaman tentang version control (Git/GitHub)
+       <br><br>
+      Dasar SEO dan keamanan web
       </p>
     </div>
   </section>
